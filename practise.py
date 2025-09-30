c41548cead8c0aa0eeb1ab83f0845213ec334752
GOD_OF_ENTITIES <herobrineking2010@gmail.com>
2025-09-30 15:03:31 +0000

    123

---
+++ practise.py
@@ -0,0 +1,15 @@
+
+a = int(input("Enter your age: "))
+print(a)
+if a < 18:
+    print("Sorry, You are too young for this!!")
+if a >= 18:
+    print("Yup, You can go ahead")
+Username = str(input("Whats ur name?"))
+if len(Username) < 4: 
+  print("Name is too short!")
+if len(Username) > 20:
+    print("Name is too long!")
+
+
+
---
+++ side.py
@@ -0,0 +1,3 @@
+A = int(input("enter a number: "))
+B = int(input("enter another number: "))
+print("the product of the two numbers is: " , A * B)
